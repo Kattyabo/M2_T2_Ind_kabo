5427f788f3c57cee9808e5ec734abb5c8bd01d9d
Kattyabo <73205193+Kattyabo@users.noreply.github.com>
2020-11-27 11:21:46 -0300

    Delete volumen_esfera.py

--- Tareas_M2_S2/volumen_esfera.py
+++
@@ -1,8 +0,0 @@
-import bs4
-import requests
-import M2_T2_Katherine_Barrera
-
-#calculadora de volumen de radio
-
-volumen_esfera = int(input("Favor ingrese radio de la esfera: "))
-print("el resultado del volumen de la esfera es: " + str(volumen_esfera))
